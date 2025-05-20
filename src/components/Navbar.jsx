7ed0a82321f618663163180453de6c6adcad0fc3
mdmonjumon <mdmonjumon786@gmail.com>
2025-05-20 14:00:12 +0600

    design footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,55 @@
 import { Link, NavLink } from "react-router-dom";
 import logo from '../assets/logos.png'
+import { useState } from "react";
 
 
 const Navbar = () => {
 
+    const [menu, setMenu] = useState(false)
+
     const links = <>
-    <NavLink className='text-lg'>Home</NavLink>
-    <NavLink className='text-lg'>All Campaign</NavLink>
-    <NavLink className='text-lg'>Add New Campaign</NavLink>
-    <NavLink className='text-lg'>My Campaign</NavLink>
-    <NavLink className='text-lg'>My Donations</NavLink>
+        <NavLink className='text-lg'>Home</NavLink>
+        <NavLink className='text-lg'>All Campaign</NavLink>
+        <NavLink className='text-lg'>Add New Campaign</NavLink>
+        <NavLink className='text-lg'>My Campaign</NavLink>
+        <NavLink className='text-lg'>My Donations</NavLink>
 
     </>
 
     return (
-        <div className="navbar">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+        <div>
+            <div className="navbar max-w-[1440px] mx-auto">
+                <div className="navbar-start">
+                    <div>
+                        <div onClick={()=>setMenu(!menu)} tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                        
+                            tabIndex={0}
+                            className={`lg:hidden absolute  flex flex-col gap-3 w-40 ${menu? 'left-0 bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow min-h-[calc(100vh-90px)]':'-left-60'}`}>
+                            {links}
+                        </ul>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        {links}
-                    </ul>
+
+                    <img className="h-20 w-52 size-full object-cover" src={logo} alt="logo" />
                 </div>
                 
-                <img className="h-20 w-1/2 size-full object-cover" src={logo} alt="logo" />
+                <div className="navbar-end gap-3">
+                    <Link className="text-lg">Register</Link>
+                    <Link className="text-lg">Login</Link>
+                </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 gap-6">
+
+            <div className="hidden lg:flex items-center justify-center border bg-[#0A3B1E] text-white py-4">
+                <ul className="menu menu-horizontal max-w-[1440px] mx-auto px-1 gap-10">
                     {links}
                 </ul>
-            </div>
-            <div className="navbar-end">
-                <Link className="text-lg">Register</Link>
             </div>
         </div>
     );
 };
 
 export default Navbar;
+
+// menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow before:min-h-screen"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (
-        <div className='max-w-11/12 mx-auto'>
+        <div>
             <header>
                 <Navbar></Navbar>
             </header>
+
+
+
+            <footer>
+                <Footer></Footer>
+            </footer>
             
         </div>
     );
