606cfcec19d393a359817cd07ca7bc073b6c724c
mdmonjumon <mdmonjumon786@gmail.com>
2025-05-29 01:20:00 +0600

    all campaign data sort by descending order implement

--- src/components/RunningCampaign.jsx
+++ src/components/RunningCampaign.jsx
@@ -1,24 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 import Card from "./Card";
-import Lottie from "lottie-react";
+
+
+
 
 
 const RunningCampaign = () => {
 
     const loadedCampaigns = useLoaderData()
 
-
-
-
     return (
         <div className="max-w-[1440px] mx-auto my-10 px-3">
             <h2 className="text-5xl font-bold text-center mt-24">Running Campaign</h2>
 
-            
-
             <section className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
                 {
-                    loadedCampaigns.map(campaign=><Card campaign={campaign} key={campaign._id}></Card>)
+                    loadedCampaigns.map(campaign => <Card campaign={campaign} key={campaign._id}></Card>)
                 }
 
             </section>
--- src/pages/AllCampaigns.jsx
+++ src/pages/AllCampaigns.jsx
@@ -1,63 +1,75 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import moment from "moment";
+import { useState } from "react";
 
 
 const AllCampaigns = () => {
 
     const loadedAllCampaigns = useLoaderData()
 
-    return (
-        <div>
-            <header>
-                <Navbar></Navbar>
-            </header>
-            <main className="max-w-[1440px] mx-auto my-10 px-3">
-                <section>
-                    <div className="overflow-x-auto">
-                        <table className="table">
-                            {/* head */}
-                            <thead>
-                                <tr>
-                                    <th>Serial</th>
-                                    <th>Campaign Title</th>
-                                    <th>Minimum Donate Amount</th>
-                                    <th>Deadline</th>
-                                    <th>Email</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+    const [allCampaign, setAllCampaign] = useState(loadedAllCampaigns)
 
-                                {/* row 1 */}
-                                {
-                                    loadedAllCampaigns.map((campaign, idx) =>
-                                        <tr key={idx} className="hover:bg-base-300">
-                                            <th>{idx + 1}</th>
-                                            <td>{campaign.title}</td>
-                                            <td>{campaign.donateAmount}</td>
-                                            <td>{`${moment(campaign.deadline).format("DD MMMM YYYY")}`}</td>
-                                            <td>{campaign.userEmail}</td>
-                                            <td className="flex items-center">
-                                                {/* update button */}
-                                                <Link to={`/campaign/${campaign._id}`} className="btn btn-info"> See More</Link>
-                                            </td>
-                                        </tr>
-                                    )
-                                }
+    const handleSortByDonationAmount = () => {
 
-                            </tbody>
-                        </table>
-                    </div>
-                </section>
-            </main>
-            <footer>
-                <Footer></Footer>
-            </footer>
+        setAllCampaign([...allCampaign].sort((a, b)=> b.donateAmount - a.donateAmount))
 
-        </div>
-    );
+    }
+
+return (
+    <div>
+        <header>
+            <Navbar></Navbar>
+        </header>
+        <main className="max-w-[1440px] mx-auto my-10 px-3">
+            <section>
+                <div className="flex justify-end">
+                    <button onClick={handleSortByDonationAmount} className="btn btn-ghost">Sort by Donation</button>
+                </div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>Serial</th>
+                                <th>Campaign Title</th>
+                                <th>Minimum Donate Amount</th>
+                                <th>Deadline</th>
+                                <th>Email</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {/* row 1 */}
+                            {
+                                allCampaign.map((campaign, idx) =>
+                                    <tr key={idx} className="hover:bg-base-300">
+                                        <th>{idx + 1}</th>
+                                        <td>{campaign.title}</td>
+                                        <td>{campaign.donateAmount}</td>
+                                        <td>{`${moment(campaign.deadline).format("DD MMMM YYYY")}`}</td>
+                                        <td>{campaign.userEmail}</td>
+                                        <td className="flex items-center">
+                                            {/* update button */}
+                                            <Link to={`/campaign/${campaign._id}`} className="btn btn-info"> See More</Link>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        </main>
+        <footer>
+            <Footer></Footer>
+        </footer>
+
+    </div>
+);
 };
 
 export default AllCampaigns;
