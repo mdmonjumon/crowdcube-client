ded7edb2766b968ca6c2d8b8b3b2fe99e1f3031b
mdmonjumon <mdmonjumon786@gmail.com>
2025-05-29 02:38:33 +0600

    implement react simple typewriter

--- src/components/Invest.jsx
+++ src/components/Invest.jsx
@@ -1,18 +1,30 @@
-
+import { useTypewriter } from 'react-simple-typewriter'
 
 import investImage from '../assets/invest.jpg'
 
 const Invest = () => {
+
+
+    const [text] = useTypewriter({
+        words: ['confidence'],
+        loop: 0,
+        typeSpeed:100,
+        deleteSpeed:80,
+
+    })
+
     return (
         <div className='max-w-[1440px] mx-auto my-10 px-3 lg:flex items-center'>
             <div className='flex-1/2'>
-                <h2 className='text-4xl lg:text-7xl font-bold text-green-500'>Invest in the future, simply, safely and with confidence</h2>
+
+                <h2 className='text-4xl lg:text-7xl font-bold text-green-500'>Invest in the future, simply, safely and with<span> {text}</span></h2>
+
                 <p className='text-lg font-semibold mt-10'>Own shares in some of Europe’s most exciting high-growth, private businesses from just 100 Taka, investing alongside and on the same terms as institutional investors.</p>
             </div>
             <div className='flex-1/2'>
                 <img className='rounded' src={investImage} alt="" />
             </div>
-            
+
         </div>
     );
 };
