dc758c5f40172615d8702c372d07e4007fec70bd
mdmonjumon <mdmonjumon786@gmail.com>
2025-05-29 02:30:11 +0600

    implement react tooltip

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,24 @@
 import moment from "moment";
 import { Link, useLocation } from "react-router-dom";
+import { Tooltip } from 'react-tooltip'
 
-const Card = ({campaign}) => {
-    const {_id, photo, title, campaignType, donateAmount, deadline}= campaign;
+const Card = ({ campaign }) => {
+    const { _id, photo, title, campaignType, donateAmount, deadline } = campaign;
 
     const date = moment(deadline).format("DD MMMM YYYY");
 
     const location = useLocation()
-    
-    
+
+
     return (
         <div>
             <div className="card bg-base-100 shadow-sm">
                 <figure>
+                    <Tooltip id="my-tooltip" />
                     <img className="md:h-80 size-full object-cover"
+                        data-tooltip-id="my-tooltip"
+                        data-tooltip-content={title}
+                        data-tooltip-place="center"
                         src={photo}
                         alt={title} />
                 </figure>
@@ -22,10 +27,10 @@ const Card = ({campaign}) => {
                     <p> <span className='text-lg font-medium'>Category: </span> <span className='text-base'>{campaignType}</span></p>
                     <p> <span className='text-lg font-medium'>Deadline: </span> <span className='text-base'>{date}</span></p>
                     <p> <span className='text-lg font-medium'>Donate Amount:</span> <span className='text-lg'>{donateAmount}</span></p>
-                    
+
                     <div className="card-actions justify-start">
                         {
-                            location.pathname === '/myDonations'? '': <Link to={`/campaign/${_id}`} className="btn btn-primary">See More</Link>
+                            location.pathname === '/myDonations' ? '' : <Link to={`/campaign/${_id}`} className="btn btn-primary">See More</Link>
                         }
                     </div>
                 </div>
--- src/components/RunningCampaign.jsx
+++ src/components/RunningCampaign.jsx
@@ -6,6 +6,7 @@ import { Fade } from "react-awesome-reveal";
 
 
 
+
 const RunningCampaign = () => {
 
     const loadedCampaigns = useLoaderData()
